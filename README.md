3672d0d3c48bd1bdf75aa3fee3a3c2951f44cb7f
Álvaro Mondéjar <mondejar1994@gmail.com>
2023-02-19 19:51:48 +0100

    Mimic iterator API of unicode-linebreak (#3)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # py-unicode-linebreak
 
-Python bindings for the Rust crate [unicode-linebreak](https://crates.io/crates/unicode-linebreak).
+Python bindings for the Rust crate [unicode-linebreak].
 
 ## Installation
 
@@ -11,17 +11,32 @@ pip install unicode-linebreak
 ## Usage
 
 ```python
-from unicode_linebreak import linebreaks
+from unicode_linebreak import linebreaks, Allowed, Mandatory
 
-string = 'a b\nc\r\nd e\rf end'
+s = 'a b\nc\r\nd e\rf end'
 expected_result = [
-    (2, False), (4, True), (7, True), (9, False),
-    (11, True), (13, False), (16, True)
+    (2, Allowed), (4, Mandatory), (7, Mandatory), (9, Allowed),
+    (11, Mandatory), (13, Allowed), (16, Mandatory)
 ]
-assert linebreaks(string) == expected_result
+assert list(linebreaks(s)) == expected_result
 ```
 
-Returns a list of tuples with the index of the linebreak and a boolean indicating whether the linebreak is a mandatory break.
+Returns an iterator which iterates over tuples with the
+binary character position of the linebreak and a boolean
+indicating whether the linebreak is a mandatory break.
+
+```python
+# the end of the string is always a mandatory break
+assert list(linebreaks(s))[-1] == (0, Mandatory)
+
+# Mandatory and Allowed are just boolean values
+assert expected_result[0][1] is Allowed is False
+assert expected_result[-1][1] is Mandatory is True
+```
+
+> Note that the binary character position is not the same
+> as the readable character index, but the binary position.
+> See the Rust method on strings [`char_indices()`].
 
 ## Contribute
 
@@ -31,3 +46,6 @@ source venv/bin/activate
 pip install -r dev-requirements.txt
 maturin develop && python -m unittest
 ```
+
+[unicode-linebreak]: https://crates.io/crates/unicode-linebreak
+[`char_indices()`]: https://doc.rust-lang.org/std/primitive.str.html#method.char_indices
--- test_unicode_linebreak.py
+++ test_unicode_linebreak.py
@@ -1,39 +1,101 @@
+import types
 import unittest
 
-from unicode_linebreak import linebreaks
+from unicode_linebreak import linebreaks, Allowed, Mandatory
+
 
 class TestPyUnicodeLinebreak(unittest.TestCase):
+    def test_linebreaks_returns_generator(self):
+        string = "a b\nc\r\nd e\rf end"
+        linebreaker = linebreaks(string)
+        self.assertIs(hasattr(linebreaker, "__iter__"), True)
+        self.assertIs(hasattr(linebreaker, "__next__"), True)
+
+        self.assertEqual(next(linebreaker), (2, Allowed))
+        self.assertEqual(next(linebreaker), (4, Mandatory))
+        self.assertEqual(next(linebreaker), (7, Mandatory))
+        self.assertEqual(next(linebreaker), (9, Allowed))
+        self.assertEqual(next(linebreaker), (11, Mandatory))
+        self.assertEqual(next(linebreaker), (13, Allowed))
+        self.assertEqual(next(linebreaker), (16, Mandatory))
+
     def test_simple(self):
-        string = 'a b\nc\r\nd e\rf end'
+        string = "a b\nc\r\nd e\rf end"
         expected_result = [
-            (2, False), (4, True), (7, True), (9, False),
-            (11, True), (13, False), (16, True)
+            (2, Allowed),
+            (4, Mandatory),
+            (7, Mandatory),
+            (9, Allowed),
+            (11, Mandatory),
+            (13, Allowed),
+            (16, Mandatory),
         ]
-        self.assertListEqual(linebreaks(string), expected_result)
+        self.assertListEqual(list(linebreaks(string)), expected_result)
 
     def test_scriptio_continua_linebreaks(self):
         string = (
-            '支持常见的温度传感器（例如，常见的热敏电阻、'
-            'AD595、AD597、AD849x、PT100、PT1000、'
-            'MAX6675、MAX31855、MAX31856、MAX31865、'
-            'BME280、HTU21D和LM75）。'
-            '还可以配置自定义热敏电阻和自定义模拟温度传感器。'
+            "支持常见的温度传感器（例如，常见的热敏电阻、"
+            "AD595、AD597、AD849x、PT100、PT1000、"
+            "MAX6675、MAX31855、MAX31856、MAX31865、"
+            "BME280、HTU21D和LM75）。"
+            "还可以配置自定义热敏电阻和自定义模拟温度传感器。"
         )
         expected_result = [
-            (3, False), (6, False), (9, False), (12, False),
-            (15, False), (18, False), (21, False), (24, False),
-            (27, False), (30, False), (36, False), (42, False),
-            (45, False), (48, False), (51, False), (54, False),
-            (57, False), (60, False), (66, False), (74, False),
-            (82, False), (91, False), (99, False), (108, False),
-            (118, False), (129, False), (140, False), (151, False),
-            (160, False), (166, False), (169, False), (179, False),
-            (182, False), (185, False), (188, False), (191, False),
-            (194, False), (197, False), (200, False), (203, False),
-            (206, False), (209, False), (212, False), (215, False),
-            (218, False), (221, False), (224, False), (227, False),
-            (230, False), (233, False), (236, False), (239, False),
-            (242, False), (245, False), (251, True)
+            (3, Allowed),
+            (6, Allowed),
+            (9, Allowed),
+            (12, Allowed),
+            (15, Allowed),
+            (18, Allowed),
+            (21, Allowed),
+            (24, Allowed),
+            (27, Allowed),
+            (30, Allowed),
+            (36, Allowed),
+            (42, Allowed),
+            (45, Allowed),
+            (48, Allowed),
+            (51, Allowed),
+            (54, Allowed),
+            (57, Allowed),
+            (60, Allowed),
+            (66, Allowed),
+            (74, Allowed),
+            (82, Allowed),
+            (91, Allowed),
+            (99, Allowed),
+            (108, Allowed),
+            (118, Allowed),
+            (129, Allowed),
+            (140, Allowed),
+            (151, Allowed),
+            (160, Allowed),
+            (166, Allowed),
+            (169, Allowed),
+            (179, Allowed),
+            (182, Allowed),
+            (185, Allowed),
+            (188, Allowed),
+            (191, Allowed),
+            (194, Allowed),
+            (197, Allowed),
+            (200, Allowed),
+            (203, Allowed),
+            (206, Allowed),
+            (209, Allowed),
+            (212, Allowed),
+            (215, Allowed),
+            (218, Allowed),
+            (221, Allowed),
+            (224, Allowed),
+            (227, Allowed),
+            (230, Allowed),
+            (233, Allowed),
+            (236, Allowed),
+            (239, Allowed),
+            (242, Allowed),
+            (245, Allowed),
+            (251, Mandatory),
         ]
 
-        self.assertListEqual(linebreaks(string), expected_result)
+        self.assertListEqual(list(linebreaks(string)), expected_result)
